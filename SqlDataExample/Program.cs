50fa52a7fd4c211c7b9c3b1e4e31ff761a4f101d
jeppe frimann <jfrimannlarsen@gmail.com>
2021-09-09 13:29:42 +0200

    Add project files.

---
+++ SqlDataExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SqlDataExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SQL sql = new SQL();
+            //InsertMethod(sql);
+            Console.WriteLine("Search for:");
+            string search = Console.ReadLine();
+            //List<Person> persons = sql.select(search);
+            List<Person> persons/*FromDatetimeList*/ = sql.select(new DateTime(1809, 02, 12));
+
+            if (persons != null && persons.Count > 0)
+            {
+                foreach (var person in persons)
+                {
+                    Console.WriteLine(person.Name);
+                }
+            }
+            else Console.WriteLine("Nothing found.");
+        }
+
+        private static void InsertMethod(SQL sql)
+        {
+            Person person = new Person();
+            person.Name = "Abraham Lincoln";
+            person.dob = new DateTime(1809, 02, 12);
+            int? id = sql.insert(person);
+            if (id != null)
+                Console.WriteLine($"{person.Name}, Born {person.dob} have gotten id : {id}");
+
+            else Console.WriteLine("Something went horribly wrong!");
+        }
+    }
+}
---
+++ SqlDataExample/SQL.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlDataExample
+{
+    class SQL
+    {
+        string ConnectionString = "Data Source=.;Initial Catalog = SqlDataDB; Integrated Security = True";
+        public int? insert(Person person)
+        {
+
+            // Prepare a proper parameterized query 
+            string sql = "Insert into person([name], dob) output inserted.id values(@name,@dob)";
+
+            // Create the connection (and be sure to dispose it at the end)
+            using (SqlConnection cnn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    // Open the connection to the database. 
+                    // This is the first critical step in the process.
+                    // If we cannot reach the db then we have connectivity problems
+                    cnn.Open();
+
+                    // Prepare the command to be executed on the db
+                    using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                    {
+                        // Create and set the parameters values 
+                        cmd.Parameters.Add("@name", SqlDbType.NVarChar).Value = person.Name;
+                        cmd.Parameters.Add("@dob", SqlDbType.DateTime).Value = person.dob;
+
+                        // Let's ask the db to execute the query
+                        int id = (int)cmd.ExecuteScalar();
+
+                        return id;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // We should log the error somewhere, 
+                    // for this example let's just show a message
+                    Console.WriteLine("ERROR:" + ex.Message);
+                    return null;
+                }
+            }
+        }
+    
+        internal List <Person> select(string search)
+        {
+            List<Person> personList = new List<Person>();
+            string sql = $"Select * from Person where [name] like '%{search}%'";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        //Console.WriteLine($"{reader[0]} - {reader[1]} - {reader[2]}");
+                        personList.Add(new Person()
+                        {
+                            Id = (int)reader[0],
+                            Name = (string)reader[1],
+                            dob = (DateTime)reader[2]
+                        });
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    // We should log the error somewhere, 
+                    // for this example let's just show a message
+                    Console.WriteLine("ERROR:" + ex.Message);
+                    return null;
+                }
+                
+            }
+            return personList;
+
+        }
+        internal List<Person> select(DateTime datetime)
+        {
+            List<Person> personList = new List<Person>();
+            string sql = $"Select * from Person where dob = @dob";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, connection);
+                    cmd.Parameters.Add("@dob", SqlDbType.DateTime).Value = datetime;
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        //Console.WriteLine($"{reader[0]} - {reader[1]} - {reader[2]}");
+                        personList.Add(new Person()
+                        {
+                            Id = (int)reader[0],
+                            Name = (string)reader[1],
+                            dob = (DateTime)reader[2]
+                        });
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    // We should log the error somewhere, 
+                    // for this example let's just show a message
+                    Console.WriteLine("ERROR:" +ex.GetType() + "\n" + ex.Message);
+                    return null;
+                }
+
+            }
+            return personList;
+
+        }
+    }
+}
